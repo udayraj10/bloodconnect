6a09d69552c0ee559810ed90b158dd43625e809e
udayraj10 <cudayabi44@gmail.com>
2026-07-22 21:59:38 +0530

    refactor(axios): update hardcode baseUrl with env baseUrl

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: API_BASE_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
