96399b2d10e44f78ec9dd220413760cea220df76
udayraj10 <cudayabi44@gmail.com>
2026-07-26 13:56:33 +0530

    chore: configure vite to drop console statements in production build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { qrcode } from "vite-plugin-qrcode"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), qrcode()],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 })
