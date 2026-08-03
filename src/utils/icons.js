b32b72b0f7fe641465cda3a4fc380b4c254108a9
udayraj10 <cudayabi44@gmail.com>
2026-08-03 22:48:17 +0530

    feat: add centralized icons utility file

---
+++ src/utils/icons.js
@@ -0,0 +1,28 @@
+export {
+  Bloodtype,
+  PendingActions,
+  TaskAlt as Task,
+  Cancel,
+  Assignment,
+  Verified,
+} from "@mui/icons-material"
+
+export {
+  MdOutlineLocationOn as Location,
+  MdOutlineReport as Report,
+} from "react-icons/md"
+
+export {
+  FiCalendar as Calendar,
+  FiUserCheck as UserCheck,
+  FiClock as Clock,
+  FiPhone as Phone,
+  FiMessageSquare as Message,
+} from "react-icons/fi"
+
+export {
+  FaHandHoldingHeart as Heart,
+  FaRegThumbsUp as ThumbsUp,
+  FaHashtag as Tag,
+  FaInfoCircle as Info,
+} from "react-icons/fa"
